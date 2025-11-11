e45a91a6bb61be4d766e48e9c78da05f32d557ea
satyam280610 <75922490+satyam280610@users.noreply.github.com>
2025-11-11 13:11:37 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Happy Birthday!",
-  description: "An animated birthday surprise filled with emotions, words from the heart, and a letter that types itself — just for you."
+  title: "Happy Anniversary!",
+  description: "An animated Anniversary surprise filled with emotions, words from the heart, and a letter that types itself — just for you."
 };
 
 export default function RootLayout({ children }) {
